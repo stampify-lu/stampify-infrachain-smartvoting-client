c49d22bafa7d51fe9ffa38b41f2066f136bfbd5a
Grégoire Mathonet <greg.mathonet@gmail.com>
2020-09-24 09:17:38 +0200

    auto set

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -30,7 +30,7 @@ import { User } from 'src/app/shared/models/user';
     }
 
     getCampagnResult(campagn: Meeting) {
-        if(new Date() < new Date(campagn.timeEnd)) return 'Unknown yet';
+        if(new Date() < new Date(campagn.timeEnd) && !campagn.timeFrozen) return 'Unknown yet';
         if(!this.resultPromises[campagn.id]) {
             this.resultPromises[campagn.id] = Promise.all([
                 this.apiService.getVotesFor(campagn),
